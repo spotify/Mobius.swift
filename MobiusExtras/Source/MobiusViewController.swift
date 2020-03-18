cade3d0e296bf4a0c589944551176c539d7fa352
Jesper Sandström <jesands@spotify.com>
2020-03-18 12:11:54 +0100

    Add the ability to automatically manage the lifecycle of a MobiusController in  UIViewController

---
+++ MobiusExtras/Source/MobiusViewController.swift
@@ -0,0 +1,112 @@
+// Copyright (c) 2019 Spotify AB.
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import MobiusCore
+import UIKit
+
+public extension UIViewController {
+    /// Attach a `MobiusController` to this `UIViewController`.
+    /// A `Connection` is returned which can be used to post events to the loop.
+    /// The `MobiusController` will start when this function is called, and stop when this `UIViewController` is
+    /// initialized or when the returned `Connection` is disposed.
+    ///
+    /// Note: You should not be calling `start`, `connectView`, `stop`, or `disconnectView` on the `MobiusController`
+    /// when using this extension.
+    ///
+    /// Note: The `MobiusController` will be placed in a `UIViewController` and added as a child `UIViewController` to
+    ///  this `UIViewController`.
+    ///
+    /// - Parameter controller: The `MobiusController` that shuold be attached
+    /// - Parameter onModelChange: A closure which is called whenever the loop's model changes.
+    func useMobius<Model, Event, Effect>(
+        controller: MobiusController<Model, Event, Effect>,
+        modelChanged onModelChange: @escaping (Model) -> Void
+    ) -> Connection<Event> {
+        let holder = MobiusHolder(controller: controller, onModelChange: onModelChange)
+        addChild(holder)
+        holder.didMove(toParent: self)
+        return Connection(
+            acceptClosure: { [unowned holder] event in
+                holder.handleEvent(event)
+            },
+            disposeClosure: { [unowned holder] in
+                holder.willMove(toParent: nil)
+                holder.removeFromParent()
+            }
+        )
+    }
+}
+
+private class MobiusHolder<Model, Event, Effect>: UIViewController {
+    private let controller: MobiusController<Model, Event, Effect>
+    private let weakConnectableDelegate: WeakConnectableDelegate<Model>
+    private let weakConnectable: WeakConnectable<Model, Event>
+
+    init(
+        controller: MobiusController<Model, Event, Effect>,
+        onModelChange: @escaping (Model) -> Void
+    ) {
+        self.controller = controller
+        self.weakConnectable = WeakConnectable()
+        self.weakConnectableDelegate = WeakConnectableDelegate<Model>(
+            onModelChange: { model in
+                onModelChange(model)
+            }
+        )
+        self.weakConnectable.delegate = self.weakConnectableDelegate
+        super.init(nibName: nil, bundle: nil)
+        controller.connectView(weakConnectable)
+        controller.start()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func handleEvent(_ event: Event) {
+        weakConnectable.send(event)
+    }
+
+    deinit {
+        if controller.running {
+            controller.stop()
+            controller.disconnectView()
+        }
+    }
+}
+
+private final class WeakConnectable<Model, Event>: ConnectableClass<Model, Event> {
+    weak var delegate: WeakConnectableDelegate<Model>?
+
+    override func handle(_ model: Model) {
+        delegate?.onModelChange(model)
+    }
+
+    override func onDispose() {
+        delegate = nil
+    }
+}
+
+private final class WeakConnectableDelegate<Model> {
+    let onModelChange: (Model) -> Void
+
+    init(onModelChange: @escaping (Model) -> Void) {
+        self.onModelChange = onModelChange
+    }
+}
---
+++ MobiusExtras/Test/MobiusViewControllerTests.swift
@@ -0,0 +1,80 @@
+// Copyright (c) 2019 Spotify AB.
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import Foundation
+import MobiusCore
+import MobiusExtras
+import Nimble
+import Quick
+
+final class MobiusViewControllerTests: QuickSpec {
+    override func spec() {
+        context("View Controller using Mobius") {
+            it("starts the Mobius Controller when the View Controller is initialized") {
+                let viewController = ViewController(onModelChange: { _ in })
+
+                expect(viewController.controller.running).to(beTrue())
+                expect(viewController.children.count).to(equal(1))
+            }
+
+            it("stops the Mobius Controller when the View Controller's connection is disposed") {
+                let viewController = ViewController(onModelChange: { _ in })
+
+                viewController.connection.dispose()
+                expect(viewController.controller.running).to(beFalse())
+                expect(viewController.children.count).to(equal(0))
+            }
+ 
+            it("forwards model changes to `onModelChange`") {
+                var model = ""
+                let viewController = ViewController(onModelChange: { newModel in
+                    model = newModel
+                })
+
+                viewController.connection.accept("1")
+                viewController.connection.accept("2")
+                viewController.connection.accept("3")
+
+                expect(model).toEventually(equal("123"))
+            }
+        }
+    }
+}
+
+private let update = Update<String, String, String> { model, event in
+    .next(model + event)
+}
+private let effectHandler = EffectRouter<String, String>().asConnectable
+private class ViewController: UIViewController {
+    let controller = Mobius.loop(update: update, effectHandler: effectHandler)
+        .makeController(from: "")
+    var connection: Connection<String>!
+
+    init(onModelChange: @escaping (String) -> Void) {
+        super.init(nibName: nil, bundle: nil)
+        connection = useMobius(
+            controller: controller,
+            modelChanged: onModelChange
+        )
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
