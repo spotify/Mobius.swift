97a962e1fbbacf38734e1c02507b124a8eba13b7
Jesper Sandström <jesands@spotify.com>
2020-03-30 14:09:59 +0200

    Import foundation

--- MobiusExtras/Source/MobiusLoopController.swift
+++ MobiusExtras/Source/MobiusLoopController.swift
@@ -19,8 +19,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+import Foundation
 import MobiusCore
-import UIKit
 
 public extension Mobius.Builder {
     func makeLoopController(
