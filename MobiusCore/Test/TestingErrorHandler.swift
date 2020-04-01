547ae34e7779c791696408b45f1b01828b5a0e0e
Jens Ayton <ahruman@spotify.com>
2020-04-01 11:26:11 +0200

    Improve comments in TestingErrorHandler.swift

--- MobiusCore/Test/TestingErrorHandler.swift
+++ MobiusCore/Test/TestingErrorHandler.swift
@@ -4,9 +4,9 @@ import MobiusThrowableAssertion
 import Nimble
 import Quick
 
-/// Nimble predicate which passes if the expression invokes `MobiusHooks.effectHandler`.
+/// Nimble predicate which passes if the expression invokes `MobiusHooks.errorHandler`.
 ///
-/// The effect handler is overridden before each test in a QuickConfiguration object. If the test has its own override,
+/// The error handler is overridden before each test in a QuickConfiguration object. If the test has its own override,
 /// for example in `beforeEach`, that will take precedence and this predicate won’t work.
 ///
 /// - Parameter capture: An optional block which is invoked with the message, file and line of the error invocation.
@@ -15,6 +15,7 @@ public func raiseError<Out>(capture: ((String, String, UInt) -> Void)? = nil) ->
     return Predicate { actualExpression in
         let message = ExpectationMessage.expectedTo("throw an assertion")
 
+        // Evaluate the expression under test, and capture any assertion _or_ Swift error
         var thrownError: Error?
         let assertion = MobiusThrowableAssertion.catch {
             do {
@@ -24,10 +25,7 @@ public func raiseError<Out>(capture: ((String, String, UInt) -> Void)? = nil) ->
             }
         }
 
-        if let assertion = assertion, let capture = capture {
-            capture(assertion.message, assertion.file, assertion.line)
-        }
-
+        // If we got a Swift error, that’s not an assertion and the test failed
         if let error = thrownError {
             return PredicateResult(
                 bool: false,
@@ -35,11 +33,18 @@ public func raiseError<Out>(capture: ((String, String, UInt) -> Void)? = nil) ->
             )
         }
 
+        // If we got an assertion and a `capture` block was passed, invoke it
+        if let assertion = assertion, let capture = capture {
+            capture(assertion.message, assertion.file, assertion.line)
+        }
+
         return PredicateResult(bool: assertion != nil, message: message)
     }
 }
 
 private class ErrorHandlerConfiguration: QuickConfiguration {
+    /// This is run before any Quick tests and registers `beforeEach`/`afterEach` handlers that run “outside” those
+    /// set up by the tests.
     override class func configure(_ configuration: Configuration) {
         configuration.beforeEach {
             MobiusHooks.setErrorHandler { message, file, line in
