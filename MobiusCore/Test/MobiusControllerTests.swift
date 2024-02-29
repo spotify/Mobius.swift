315a69a3dbf85eccd12663d25e37361b6a7e1e03
Manuel Burghard <234355+mjburghard@users.noreply.github.com>
2024-02-29 16:17:10 +0100

    Update copyright (#208)

--- MobiusCore/Test/MobiusControllerTests.swift
+++ MobiusCore/Test/MobiusControllerTests.swift
@@ -1,4 +1,4 @@
-// Copyright 2019-2022 Spotify AB.
+// Copyright 2019-2024 Spotify AB.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
