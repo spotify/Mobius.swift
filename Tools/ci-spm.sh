f03688c3ed6d17900ef8591cf3738c4cd8985799
kmcbride <kmcbride@users.noreply.github.com>
2025-03-10 15:32:44 -0700

    Fix code coverage processing

--- Tools/ci-spm.sh
+++ Tools/ci-spm.sh
@@ -6,10 +6,5 @@ source "$(dirname "$0")/helpers.sh"
 # SPM build and test
 swift test --enable-code-coverage --sanitize=thread || fail "SPM Test Failed"
 
-# h4x to trick codecov
-for bundle in .build/debug/*.xctest ; do
-  parent="$(dirname "$bundle")"
-  ln -sf "$PWD/$bundle" "${parent}/codecov"
-done
-
-process_coverage -D ".build/debug" -F "macspm"
+# Process codecov
+process_coverage -D ".build/debug/codecov" -F "macspm"
